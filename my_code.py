aea591f034bd0a2b839dac9da1e9b5bc555c79e7
yesak1 <23kkazliner@athenian.org>
2020-09-12 04:05:46 +0000

    updated after comments.

--- my_code.py
+++ my_code.py
@@ -4,4 +4,4 @@
 # Write a program that asks for the user's name and another piece of information.Then prints a response using both of the inputs.
 name=input("what is your name?")
 info=input(f"hi {name} what's your favorite color?")
-print ("nice!")
+print (f"{name}'s favorite color is {info}!")
